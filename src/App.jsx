14d06bc5d4cf61c2b09f3bbaa4ce98ba21087bf3
shibil-06 <sibilmanappully@gmail.com>
2024-12-09 23:40:19 +0530

    massage

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("white")
+
+
+  return (
+    <>
+    
+    <div className='div2'>
+      <div className='div3'style={{backgroundColor:color}} >
+
+      </div>
+      <button onClick={()=>setColor("red")} className='red'>red</button>
+      <button  onClick={()=>setColor("blue")} className='blue'>blue</button>
+      <button  onClick={()=>setColor("green")} className='green'>green</button>
+      <button onClick={()=>setColor("yellow")} className='yellow'>yellow</button>
+      <button onClick={()=>setColor("orange")} className='orange'>orange</button>
+    </div>
+    </>
+  )
+}
+
+export default App
